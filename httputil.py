afd16ae9ccc15ecc06f17d324010a8c0a0bc6e86
Jeff Garzik <jeff@bloq.com>
2017-08-12 23:55:17 +0000

    add timeout to fetches

--- httputil.py
+++ httputil.py
@@ -5,7 +5,7 @@ def jsonfetch(opts):
 	if 'headers' not in opts:
 		opts['headers'] = {}
 
-	r = requests.get(opts['url'], headers=opts['headers'])
+	r = requests.get(opts['url'], headers=opts['headers'], timeout=10)
 	if r.status_code != 200:
 		return None
 	
