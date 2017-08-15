e9748fea642844fd0feee53f4bf4d401df03ecea
Jeff Garzik <jeff@bloq.com>
2017-08-15 19:59:54 +0000

    Move Decimal utilities to separate module

---
+++ decutils.py
@@ -0,0 +1,23 @@
+
+import json
+from decimal import Decimal, ROUND_FLOOR, getcontext
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
+
+def decimalstr(d):
+	s = '{0:f}'.format(d)
+	return s
+
+def decimaltrunc(d, n):
+	s = decimalstr(d)
+	before_dec, after_dec = s.split('.')
+	truncStr = '.'.join((before_dec, after_dec[0:n]))
+	return Decimal(truncStr)
+
+def jsonDumps(obj):
+	return json.dumps(obj, cls=DecimalEncoder)
+
